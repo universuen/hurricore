05fddaeacc895235fb97d5a17303ef7cedfd72d0
universuen <universuen@gmail.com>
2024-04-10 19:52:37 +0800

    modified:   examples/gan_for_cat/hooks/img_peek_hook.py

--- examples/gan_for_cat/hooks/img_peek_hook.py
+++ examples/gan_for_cat/hooks/img_peek_hook.py
@@ -32,7 +32,7 @@ class ImgPeekHook(HookBase):
                 images = g_model(self.trainer.ctx.z.to(self.trainer.accelerator.device)).detach()
             images = (images + 1) / 2
             image_grid = make_grid(images, nrow=3)
-            filename = self.folder_path / f"results_at_step_{self.trainer.ctx.global_step}.png"
+            filename = self.folder_path / f"results_at_step_{self.trainer.ctx.global_step + 1}.png"
             if self.trainer.accelerator.is_main_process:
                 save_image(image_grid, filename)
             TensorBoardHook.msg_queue.append(
@@ -41,7 +41,7 @@ class ImgPeekHook(HookBase):
                     {
                         'tag': 'Generated Images',
                         'img_tensor': image_grid,
-                        'global_step': self.trainer.ctx.global_step,
+                        'global_step': self.trainer.ctx.global_step + 1,
                     }
                 )
             )
