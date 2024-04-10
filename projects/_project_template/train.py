a880821a164c5fa9b72a52c2dfef76c14a40fad0
universuen <52519513+universuen@users.noreply.github.com>
2024-04-10 16:15:58 +0800

    Update train.py

--- projects/_project_template/train.py
+++ projects/_project_template/train.py
@@ -5,40 +5,47 @@ from torch.optim import AdamW
 from torch.optim.lr_scheduler import CosineAnnealingLR
 from accelerate import Accelerator
 
-from hurricane.utils import Logger, launch, log_all_configs
+from hurricane.utils import Logger, launch, log_all_configs, import_config
 
-from configs.default import (
-    LoggerConfig,
-    AcceleratorConfig,
-    DataLoaderConfig,
-    OptimizerConfig,
-    TrainerConfig,
-    LaunchConfig,
-)
+# import config from module path
+config = import_config('configs.default')
+
+""" Optional:
+import config from file path
+`config = import_config('projects/_project_template/configs/default.py')`
+import config from url
+`config = import_config('https://raw.githubusercontent.com/universuen/hurricane/main/projects/_project_template/configs/default.py')`
+"""
 
 
 def main():
-    logger = Logger(**LoggerConfig())
-    accelerator = Accelerator(**AcceleratorConfig())
+    # setup logger and accelerator
+    logger = Logger(**config.LoggerConfig())
+    accelerator = Accelerator(**config.AcceleratorConfig())
     if accelerator.is_main_process:
         log_all_configs(logger)
+    # setup dataset, model and dataloader
     with accelerator.main_process_first():
         dataset = ...
         model = ...
     data_loader = torch.utils.data.DataLoader(
         dataset=dataset, 
-        **DataLoaderConfig(),
+        **config.DataLoaderConfig(),
     )
+    # setup optimizer and lr scheduler
     optimizer = AdamW(
         params=model.parameters(), 
-        **OptimizerConfig(),
+        **config.OptimizerConfig(),
     )
+    num_steps_per_epoch = len(data_loader) // config.AcceleratorConfig().gradient_accumulation_steps
+    num_epochs = config.TrainerConfig().epochs
     scheduler = CosineAnnealingLR(
         optimizer=optimizer,
-        T_max=(len(data_loader) // AcceleratorConfig().gradient_accumulation_steps) * TrainerConfig().epochs,
+        T_max=num_steps_per_epoch * num_epochs,
     )
+    # setup trainer and run
     trainer = ...
     trainer.run()
 
 if __name__ == '__main__':
-    launch(main, **LaunchConfig())
+    launch(main, **config.LaunchConfig())
