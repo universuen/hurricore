2168a23c93d34fb7fa667e8257b23beb652a49f2
universuen <52519513+universuen@users.noreply.github.com>
2024-04-10 16:12:26 +0800

    Update inference.py

--- examples/gan_for_cat/inference.py
+++ examples/gan_for_cat/inference.py
@@ -3,30 +3,33 @@ import torch
 import cv2
 from tqdm import tqdm
 
-from models import Generator
-from configs.for_256px import GeneratorConfig, PathConfig
+from hurricane.utils import import_config
 
-CKPT_PATH = PathConfig().checkpoints / 'ckpt_step_45000'
+from models import Generator
+
+config = import_config('configs.for_256px')
+
+CKPT_PATH = config.PathConfig().checkpoints / 'ckpt_step_45000'
 STEP = 72
 NUM_CATS = 5
 
 if __name__ == '__main__':
     # load model
-    generator = Generator(**GeneratorConfig()).to('cpu')
+    generator = Generator(**config.GeneratorConfig()).to('cpu')
     model_path = CKPT_PATH / 'pytorch_model.bin'
     generator.load_state_dict(torch.load(model_path, map_location='cpu'))
     generator.eval()
 
     # Set up video writer
-    video_filename = PathConfig().data / 'interpolation.mp4'
+    video_filename = config.PathConfig().data / 'interpolation.mp4'
     # Define the codec and create VideoWriter object
     fourcc = cv2.VideoWriter_fourcc(*'mp4v')
     video_writer = cv2.VideoWriter(str(video_filename), fourcc, 24.0, (256, 256))
     
     # create interpolation
     cnt = 0
-    start_z = torch.randn(1, GeneratorConfig().z_dim).to('cpu')
-    end_z = torch.randn(1, GeneratorConfig().z_dim).to('cpu')
+    start_z = torch.randn(1, config.GeneratorConfig().z_dim).to('cpu')
+    end_z = torch.randn(1, config.GeneratorConfig().z_dim).to('cpu')
     while cnt < NUM_CATS:
         z_diff = (end_z - start_z) / STEP
         for i in tqdm(range(STEP)):
@@ -40,7 +43,7 @@ if __name__ == '__main__':
             video_writer.write(frame)
         cnt += 1
         start_z = end_z
-        end_z = torch.randn(1, GeneratorConfig().z_dim).to('cpu')
+        end_z = torch.randn(1, config.GeneratorConfig().z_dim).to('cpu')
 
     # Release the video writer
     video_writer.release()
