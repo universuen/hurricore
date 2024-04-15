6164aa260f5cde44d690dad71becfe156bbe9998
universuen <universuen@gmail.com>
2024-04-15 17:04:39 +0000

    update

--- examples/diffusion_for_cat_from_scratch/peek_noise_scheduler.py
+++
@@ -1,9 +0,0 @@
-import _path_setup  # noqa: F401
-
-from hurricane.utils import import_config
-
-if __name__ == '__main__':
-    config = import_config('configs.default')
-    
-    
-    
--- examples/diffusion_for_cat_from_scratch/unet.py
+++ examples/diffusion_for_cat_from_scratch/unet.py
@@ -125,13 +125,10 @@ class UpSampling(nn.Module):
 
 
 def sinusoidal_embedding(n, d):
-    # Returns the standard positional embedding
     embedding = torch.tensor([[i / 10_000 ** (2 * j / d) for j in range(d)] for i in range(n)])
     sin_mask = torch.arange(0, n, 2)
-
     embedding[sin_mask] = torch.sin(embedding[sin_mask])
     embedding[1 - sin_mask] = torch.cos(embedding[sin_mask])
-
     return embedding
 
 
