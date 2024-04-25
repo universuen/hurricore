d9a98675d7c81fdbf053a1520da06c423395ab02
universuen <universuen@gmail.com>
2024-04-25 18:27:20 +0800

    update

--- kill.sh
+++ kill.sh
@@ -1,1 +1,2 @@
-kill $(ps aux | grep "train.py" | grep -v grep | awk '{print $2}')
+# Kill all processes that match the pattern "train.*\.py"
+kill $(ps aux | grep -E "train.*\.py" | grep -v grep | awk '{print $2}')
